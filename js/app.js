1eb89957ac217099a35aa47a2c717df07fe0f0b6
gitaaulia05 <gitaauliahafid@gmail.com>
2024-02-03 12:53:45 +0700

    FirstCommit

---
+++ js/app.js
@@ -0,0 +1,14 @@
+// START NAVBAR SCROLL
+
+// window.onscroll = function(){
+//     const navbars = document.querySelector('.navbar');
+//     const fixednav = navbars.offsetTop;
+
+//     if(window.pageYOffset > fixednav){
+//         navbars.classList.add('navbar-fixed');
+//     }else{
+//         navbars.classList.remove('navbar-fixed');
+//     }
+// }
+
+// END NAVBAR SCROLL
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    extend: {
+      colors: {
+        abu: '#F5F5F5',
+        kuning: '#FFD873',
+        abut:'#B6B6B6',
+      } ,
+      fontFamily: {
+        roboto : ['Roboto, sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
+
